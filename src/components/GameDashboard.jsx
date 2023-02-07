4366ff9dcb2c8c61b7d0ff46e20319efdfe5c2fb
Ashley Valentine <avalentinexo@gmail.com>
2023-02-07 15:31:11 -0500

    removed note

--- src/components/GameDashboard.jsx
+++ src/components/GameDashboard.jsx
@@ -1,15 +1,6 @@
-// import { useEffect } from 'react'
 import Canvas from './Canvas'
 
 function GameDashboard({ setRoom }) {
-  // useEffect(() => {
-  //   if (socket === null) return
-
-  //   socket.on('room-data', () => {
-  //     console.log('You triumphed')
-  //   })
-  // }, [])
-
   function handleLeaveRoom() {
     setRoom(null)
   }
